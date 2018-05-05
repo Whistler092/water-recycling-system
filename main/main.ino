c840c1a35054f6fb8e896bdc7c2197767d7b465b
Ramiro Andrés bedoya <rbedoyae@intergrupo.com>
2018-05-05 14:43:58 -0500

    updated with wifi

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,21 @@
 #include <ESP8266HTTPClient.h>
 
-#include <WiFiServerSecure.h>
-#include <WiFiClientSecure.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiUdp.h>
-#include <ESP8266WiFiType.h>
-#include <ESP8266WiFiAP.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiSTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
 
 #include <EEPROM.h>
 
-#include <ESP8266WebServer.h>
-#include <ESP8266WebServerSecure.h>
- 
+/*
+ HC-SR04 Ping distance sensor]
+ VCC to arduino 5v GND to arduino GND
+ Echo to Arduino pin 13 Trig to Arduino pin 12
+ Red POS to Arduino pin 11
+ Green POS to Arduino pin 10
+ 560 ohm resistor to both LED NEG and GRD power rail
+ More info at: http://goo.gl/kJ8Gl
+ Original code improvements to the Ping sketch sourced from Trollmaker.com
+ Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+ */
 
 #define pintrigger 4
 #define pinecho 5
@@ -62,7 +60,7 @@ void loop() {
       Serial.println("el agua esta sucia");
     }
     else {
-      Serial.println("pro");
+      Serial.println("Reciclando ");
       electrovalve();
     }
   
@@ -91,17 +89,22 @@ void electrovalve(){
   if (distance < 4) {  // This is where the LED On/Off happens
     digitalWrite(pinelectrovalve,HIGH); // When the Red condition is met, the Green LED should turn off
     digitalWrite(led2,LOW);
+    sendData((String)"Lleno " + distance + "CM");
   }
   else {
     digitalWrite(pinelectrovalve,LOW);
     digitalWrite(led2,HIGH);
   }
+  
   if (distance >= 200 || distance <= 0){
     Serial.println("Fuera de Rango");
+    sendData((String)"Fuera de Rango " + distance);
   }
   else {
     Serial.print(distance);
     Serial.println(" cm");
+    sendData((String)"llenando " + distance);
+
   }
 }
 
@@ -331,10 +334,6 @@ void createWebServer(int webtype){
     
   }
 }
-
-
-
-
 
 
 
